f1f08e5940f2507fac3182b40209143324c8b8ef
刘理 <liuli@liulideMacBook-Pro.local>
2021-08-20 16:26:18 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: '小理哥的小博客',
+  description: '这是我的第一个 VuePress 站点',
+
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '前端',
+        link: '/front',
+        children: [
+           {
+            text: 'html',
+            link: '/html',
+          },
+          {
+            text: 'js',
+            link: '/js',
+          },
+          {
+            text: 'css',
+            link: '/css',
+          },
+        ]
+      },
+      {
+        text: '后端',
+        link: '/after',
+        children: [
+          {
+            text: 'node',
+            link: '/node',
+          },
+          {
+            text: 'koa',
+            link: '/koa',
+          },
+        ]
+      }
+    ],
+  },
+}
